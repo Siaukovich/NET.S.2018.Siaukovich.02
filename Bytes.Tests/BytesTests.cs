3074b08c1e0b1ba97f9ece6a3a6f018d3aa8855a
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-06-27 18:00:35 +0300

    NUnit tests were added.

---
+++ Bytes.Tests/BytesTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Bytes.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BytesTests
+    {
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        public int InsertNumber_ValidInput_ValidResult(int numberSource, int numberIn, int i, int j) =>
+            Bytes.InsertNumber(numberSource, numberIn, i, j);
+
+        [Test]
+        public void InsertNumber_IGreaterThanJ_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Bytes.InsertNumber(15, 15, 11, 5));
+        }
+
+        [Test]
+        public void InsertNumber_JGreaterThan32_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Bytes.InsertNumber(15, 15, 35, 33));
+        }
+    }
+}
