9a8dc56898183f8056335cea1d46e65cc25154a4
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-06-27 17:51:49 +0300

    MS tests for InsertNumbers were added.

---
+++ Bytes.MSTests/BytesMSTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Bytes.MSTests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BytesMSTests
+    {
+        [TestMethod]
+        public void InsertNumber_Passed8_15_3_8_Retunrs120()
+        {
+            int expected = 120;
+
+            int actual = Bytes.InsertNumber(8, 15, 3, 8);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertNumber_Passed8_15_0_0_Returns9()
+        {
+            int expected = 9;
+
+            int actual = Bytes.InsertNumber(8, 15, 0, 0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertNumber_Passed15_15_0_0_Returns15()
+        {
+            int expected = 15;
+
+            int actual = Bytes.InsertNumber(15, 15, 0, 0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumber_IGreaterThanJ_ThrowsArgumentOutOfRangeException() => Bytes.InsertNumber(15, 15, 11, 5);
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumber_JGreaterThan32_ThrowsArgumentOutOfRangeException() => Bytes.InsertNumber(15, 15, 35, 33);
+    }
+}
