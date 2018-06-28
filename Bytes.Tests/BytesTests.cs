3cc94a5fc6d2a1288c5f85a95129527906f36d0b
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-06-28 19:54:06 +0300

    NUnit extra tests were added.

--- Bytes.Tests/BytesTests.cs
+++ Bytes.Tests/BytesTests.cs
@@ -3,12 +3,17 @@
     using System;
     using NUnit.Framework;
 
+    /// <summary>
+    /// Class for testing class Bytes.
+    /// </summary>
     [TestFixture]
     public class BytesTests
     {
         [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
         [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
         [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(68, 2222, 5, 14, ExpectedResult = 5572)]
+        [TestCase(2222, 68, 2, 5, ExpectedResult = 2194)]
         public int InsertNumber_ValidInput_ValidResult(int numberSource, int numberIn, int i, int j) =>
             Bytes.InsertNumber(numberSource, numberIn, i, j);
 
